f21c6ee8f3d3aa7ba4596ac43b52ef81ab25d58f
Pankaj Jadhav <pankajjadhav1712@gmail.com>
2026-04-17 00:28:23 +0530

    Refactor Lagrange class by removing comments

--- NC/include/Lagrange.hpp
+++ NC/include/Lagrange.hpp
@@ -4,16 +4,10 @@
 using namespace std;
 
 class Lagrange : public Interpolation {
-
 public:
-    // Constructor: passes xs, ys up to Interpolation --> Matrix
     Lagrange(const vector<double>& xs, const vector<double>& ys);
-    // Evaluates P(x) = sum of y_i * L_i(x)
     double interpolate(double x) const override;
-    // Evaluates i-th Lagrange basis polynomial:
     double basisPoly(int i, double x) const;
-    // Implements pure virtual from Interpolation
     string methodName() const override { return "Lagrange Interpolation"; }
 };
-
 #endif      
